7edcb42226de59a9f0d60b6f19a69207694bc064
Marlen <mambalxd@gmail.com>
2026-03-22 11:47:10 +0100

    deploying backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,11 +19,13 @@ origins = [
 ]
 
 app.add_middleware(
+    app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
 )
 
 
